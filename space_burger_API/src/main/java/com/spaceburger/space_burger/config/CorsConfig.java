90052cad6db39b915b3f30e5ff77e05d58d28762
Pedro Alexandre <pedrok.santanagomes@gmail.com>
2025-07-28 23:55:48 -0300

    carousel fix

--- space_burger_API/src/main/java/com/spaceburger/space_burger/config/CorsConfig.java
+++ space_burger_API/src/main/java/com/spaceburger/space_burger/config/CorsConfig.java
@@ -21,11 +21,12 @@ public class CorsConfig implements WebMvcConfigurer {
                         "https://space-burger-dks9-pedro-alexandrs-projects.vercel.app",
                         "https://space-burger-dks9-git-main-pedro-alexandrs-projects.vercel.app/",
                         "https://space-burger-dks9-git-main-pedro-alexandrs-projects.vercel.app",
+                        "https://space-burger-dks9-fcvpbstrs-pedro-alexandrs-projects.vercel.app/",
+                        "https://space-burger-dks9-fcvpbstrs-pedro-alexandrs-projects.vercel.app",
                         "https://space-burger-dks9.vercel.app",
                         "https://space-burger-dks9.vercel.app/",
                         "http://localhost:5173",
-                        "http://127.0.0.1:5173"
-                )
+                        "http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
                 .allowCredentials(true)
